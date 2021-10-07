3212a3566d3caec0d261ee566ccc0f2d9c6ed81c
JoshGrainge <josh.grainge@hotmail.com>
2021-10-06 20:11:06 -0400

    Assignment 3

---
+++ Assignment3/Assignment3/Birds.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using A3.Eggs;
+namespace A3.Birds
+{
+	abstract class Bird
+	{
+		public static Random Rand = new Random(1);
+        public abstract Egg[] LayEggs(int numEggs);
+
+        protected Egg GetEgg(double minSize, double maxSize, double brokenChance, Egg.Colors color)
+        {
+            double eggSize = Rand.NextDouble() * (maxSize - minSize) + minSize;
+
+            if (Rand.NextDouble() <= brokenChance)
+                return new BrokenEgg(eggSize, color);
+            else
+                return new Egg(eggSize, color);
+        }
+	}
+
+	class Chicken : Bird
+	{
+        public override Egg[] LayEggs(int numEggs)
+        {
+            Egg[] eggs = new Egg[numEggs];
+            for (int i = 0; i < numEggs; i++)
+                eggs[i] = GetEgg(2, 4, 0.25, Egg.Colors.brown);
+
+            return eggs;
+        }
+    }
+
+	class Ostrich : Bird 
+	{
+        public override Egg[] LayEggs(int numEggs)
+        {
+            Egg[] eggs = new Egg[numEggs];
+            for (int i = 0; i < numEggs; i++)
+                eggs[i] = GetEgg(10, 15, 0.45, Egg.Colors.speckled);
+
+            return eggs;
+        }
+    }
+}
---
+++ Assignment3/Assignment3/Eggs.cs
@@ -0,0 +1,36 @@
+﻿namespace A3.Eggs
+{
+	class Egg
+	{
+		public enum Colors
+		{
+			speckled,
+			brown
+		}
+		public double Size { get; private set; }
+		public Colors Color { get; private set; }
+		public Egg(double size, Colors color)
+		{
+			Size = size;
+			Color = color;
+		}
+		public override string ToString()
+		{
+			return $"A {Size:0.0}cm {Color} egg";
+		}
+	}
+
+	
+	class BrokenEgg : Egg
+	{
+		// TODO: Implement for Task 2
+
+		public BrokenEgg(double size, Colors color) : base(size, color) { }
+
+        public override string ToString()
+        {
+            return $"A {Size:0.0}cm {Color} egg that is broken";
+        }
+    }
+	
+}
